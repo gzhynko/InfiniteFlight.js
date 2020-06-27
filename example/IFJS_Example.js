994b09a8be392a906eca535aef5048b4f68915fb
Kai Malcolm <kai.malcolm@icloud.com>
2020-06-27 22:50:13 +1000

    Update IFJS_Example.js

--- example/IFJS_Example.js
+++ example/IFJS_Example.js
@@ -4,8 +4,9 @@ InfiniteFlight.init(function() {
     InfiniteFlight.onMessage(function(msg) {
         console.log(msg);
     });
-    InfiniteFlight.sendCmd('Commands.FlapsFullDown', []);
+    InfiniteFlight.sendCmd('Airplane.GetState', []);
     InfiniteFlight.sendCmd('Commands.ParkingBrakes', []);
+    console.log("Commands Sent");
 }, function() {
     throw new Error('Error Connecting to Infinite Flight');
 });
