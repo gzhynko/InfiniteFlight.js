656c2bd16e61ef9f33898727b1e7bfc57ba388ca
Gleb Zhynko <gleb.zhinko@gmail.com>
2020-10-03 13:05:18 +0300

    refactor: remove commented code refactor: move all methods to the new IFTCPClient class feat: add 'setSocketTimeout' method and 'onTimeout' event feat: add 'connected' bool

--- InfiniteFlight.js
+++ InfiniteFlight.js
@@ -1,148 +1,143 @@
 var dgram = require('dgram');
 var net = require('net');
-var _addr, _port, client;
+const {PromiseSocket} = require("promise-socket")
 
-exports.init = function (success, error) {
-  try {
+exports.init = function () {
+  return new IFTCPClient();
+}
+
+class IFTCPClient {
+  constructor () {
+    this.client = new PromiseSocket(new net.Socket()); 
+    this.ipAddress = ""; 
+    this.port = 0;
+    this.connected = false;
+  }
+
+  connect (ip, port) {
+    return this.client.connect(parseInt(port), ip);
+  }
+
+  establishConnection (success, error, attempts) {
     var s = dgram.createSocket('udp4');
-    s.on('message', function(msg, rinfo) {
-      var response = JSON.parse(msg.toString());
-      if (response.Addresses[1] && response.Port) {
-        _addr = response.Addresses[1];
-        _port = 10112;
-        s.close();
-        client = new net.Socket();
-        client.connect(parseInt(_port), _addr);
-        console.log("Connected to Infinite Flight at " + _addr + ':' + _port);
-        success();
-      }
-    });
     s.bind(15000);
-  } catch {
-    error();
+
+    s.on('message', function(msg) {
+      var response = JSON.parse(msg.toString());
+
+      if (response.Addresses[1] && response.Port) {
+        this.ipAddress = response.Addresses[1];
+        this.port = 10112;
+        s.close();
+
+        this.connect(this.ipAddress, this.port).then(function(){
+          console.log("Connected to Infinite Flight at " + this.ipAddress + ':' + this.port);
+          this.connected = true;
+        }.bind(this)).catch((reason) => {
+          console.log("Connection rejected: " + reason);
+        });
+      }
+    }.bind(this));
   }
-}
 
-exports.writeBool = function (val) {
-  if (val === true) {
-    client.write(Buffer.from([01]));
-  } else {
-    client.write(Buffer.from([00]));
+  writeBool (val) {
+    if(this.connected != true) return;
+
+    if (val === true) {
+      this.client.write(Buffer.from([1]));
+    } else {
+      this.client.write(Buffer.from([0]));
+    }
   }
-}
 
-exports.writeInt = function (val) {
-  var data = Buffer.allocUnsafe(4);
-  data.writeInt32LE(val);
-  client.write(data);
-}
+  writeInt (val) {
+    if(this.connected != true) return;
 
-exports.writeFloat = function (val) {
-  var data = Buffer.allocUnsafe(4);
-  data.writeFloatLE(val);
-  client.write(data);
-}
-
-
-exports.writeDouble = function (val) {
-  var data = Buffer.allocUnsafe(4);
-  data.writeDoubleLE(val);
-  client.write(data);
-}
-
-exports.writeString = function (val) {
-  var data = new Uint8Array(val.length + 4);
-  data[0] = val.length();
-  for (var i=0;i<val.length;i++) {
-    data[i+4] = val.charCodeAt(i);
+    var data = Buffer.allocUnsafe(4);
+    data.writeInt32LE(val);
+    this.client.write(data);
   }
-  client.write(data);
-}
 
-exports.readBool = function (val) {
-  return (val.toJSON() === true);
-}
+  writeFloat (val) {
+    if(this.connected != true) return;
 
-exports.readInt = function (val) {
-  return val.readInt32LE();
-}
+    var data = Buffer.allocUnsafe(4);
+    data.writeFloatLE(val);
+    this.client.write(data);
+  }
 
-exports.readFloat = function(val) {
-  return val.readFloatLE();
-}
+  writeDouble (val) {
+    if(this.connected != true) return;
 
-exports.readDouble = function (val) {
-  return val.readDoubleLE();
-}
+    var data = Buffer.allocUnsafe(4);
+    data.writeDoubleLE(val);
+    this.client.write(data);
+  }
 
-exports.readString = function(val) {
-  return val.toString();
-}
+  writeString (val) {
+    if(this.connected != true) return;
 
-// Not working
-// exports.readLong = function(val) {
-//   return val.readInt64LE();
-// }
-// exports.getState = function (command, retType) {
-//   if (retType == null) {
-//     throw new Error("Return Type Parameter Requried for GetState Requests");
-//   }
-//   exports.writeInt(command);
-//   exports.writeBool(false);
-//   var i = 0;
-//   client.on("data", function(chunk) {
-//     if (retType == 4) {
-//       if (i == 3) {
-//         return exports.readString(chunk);
-//       }
-//     } else {
-//       if (i == 2) {
-//         if (retType == 0) {
-//           return exports.readBool(chunk);
-//         } else if (retType == 1) {
-//           return exports.readInt(chunk);
-//         } else if (retType == 2) {
-//           return exports.readFloat(chunk);
-//         } else if (retType == 3) {
-//           return exports.readDouble(chunk);
-//         } else if (retType == 5) {
-//           throw new Error("Longs Not Supported");
-//         } else {
-//           throw new Error("Invalid Return Type");
-//         }
-//       }
-//     }
-//     i++;
-//   });
-// }
+    var data = new Uint8Array(val.length + 4);
+    data[0] = val.length();
+    for (var i=0;i<val.length;i++) {
+      data[i+4] = val.charCodeAt(i);
+    }
+    this.client.write(data);
+  }
 
-// exports.setState = function (command, value, valueType) {
-//   exports.writeInt(command);
-//   exports.writeBool(true);
-//   if (valueType == 0) {
-//     exports.writeBool(value);
-//   } else if (valueType == 1) {
-//     exports.writeInt(value);
-//   } else if (valueType == 2) {
-//     exports.writeFloat(value);
-//   } else if (valueType == 3) {
-//     exports.writeDouble(value);
-//   } else if (valueType == 4) {
-//     exports.writeString(value);
-//   } else if (valueType == 5) {
-//     throw new Error("Longs Not Supported");
-//   } else {
-//     throw new Error("Invalid Value Type");
-//   }
-// }
+  readBool (val) {
+    if(this.connected != true) return;
 
-exports.runCmd = function (command) {
-  exports.writeInt(command);
-  exports.writeBool(false);
-}
+    return (val.toJSON() === true);
+  }
 
-exports.onMessage = function(callback) {
-  client.on('data', function(chunk) {
-    callback(chunk);
-  })
+  readInt (val) {
+    if(this.connected != true) return;
+
+    return val.readInt32LE();
+  }
+
+  readFloat (val) {
+    if(this.connected != true) return;
+
+    return val.readFloatLE();
+  }
+  
+  readDouble (val) {
+    if(this.connected != true) return;
+
+    return val.readDoubleLE();
+  }
+  
+  readString (val) {
+    if(this.connected != true) return;
+
+    return val.toString();
+  }
+
+  runCmd (command) {
+    writeInt(command);
+    writeBool(false);
+  }
+  
+  
+  setSocketTimeout (timeout) {
+    this.client.setTimeout(timeout);
+  }
+  
+  onMessage (callback) {
+    if(this.connected != true) return;
+
+    this.client.on('data', function(chunk) {
+      callback(chunk);
+    })
+  }
+  
+  onTimeout (callback) {
+    if(this.connected != true) return;
+
+    this.client.on('timeout', function(){
+      callback();
+    })
+  }
 }
